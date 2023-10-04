c02fa8216f71f762b8c36ef83bc651ca97ec523f
Geronimo <geronimonicolas-97@hotmail.com>
2023-10-04 18:31:52 -0300

    Mejoras estilado, ahora valores cuota ok

--- src/components/Registrador.jsx
+++ src/components/Registrador.jsx
@@ -34,7 +34,9 @@ function Registrador() {
             id: uniqid(),
             nombre: nombreCuota,
             monto: montoCompra,
-            cantCuo: cantCuotas,
+            cantCuotas: cantCuotas,
+            fechaCompra: fechaCompra,
+            categoria: categoria
         }
         setListaCuotas([...listaCuotas, nuevaCuota])
         setNombreCuota('')
@@ -69,62 +71,6 @@ function Registrador() {
         setCategoria(event.target.value);
     };
 
-    const people = [
-        {
-            name: 'Leslie Alexander',
-            email: 'leslie.alexander@example.com',
-            role: 'Co-Founder / CEO',
-            imageUrl:
-                'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Michael Foster',
-            email: 'michael.foster@example.com',
-            role: 'Co-Founder / CTO',
-            imageUrl:
-                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Dries Vincent',
-            email: 'dries.vincent@example.com',
-            role: 'Business Relations',
-            imageUrl:
-                'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: null,
-        },
-        {
-            name: 'Lindsay Walton',
-            email: 'lindsay.walton@example.com',
-            role: 'Front-end Developer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Courtney Henry',
-            email: 'courtney.henry@example.com',
-            role: 'Designer',
-            imageUrl:
-                'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: '3h ago',
-            lastSeenDateTime: '2023-01-23T13:23Z',
-        },
-        {
-            name: 'Tom Cook',
-            email: 'tom.cook@example.com',
-            role: 'Director of Product',
-            imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-            lastSeen: null,
-        },
-    ]
-
-
     return (
         <Fragment>
 
@@ -134,24 +80,25 @@ function Registrador() {
             </div>
 
             <div class="grid grid-cols-2 gap-4">
-                <div class="grid m-5 border-4 border-black p-4 rounded-lg">
+                <div class="grid m-5 border-4 border-black p-4 rounded-lg ">
                     <h2 class="text-2xl font-bold mb-4 text-center">
                         Tus cuotas por pagar
                     </h2>
-                    <div class="container " >
+                    <div class="container overflow-y-auto h-[32rem]" >
                         <ul class="item-group mx-auto">
 
-                            {
-                                listaCuotas.map(item =>
-                                    <div class="p-2">
-                                        <li class="mt-5 rounded-2xl border-2 p-3.5 hover:bg-slate-200">
+                            <div class="p-2">
+                                {
+                                    listaCuotas.map(item =>
+                                        <li key={item} class="mt-5 rounded-2xl border-2 p-3.5 hover:bg-slate-200">
 
                                             <div class="flex flex-row">
 
                                                 <div class="basis-3/4">
 
-                                                    <h3>Compra</h3>
-                                                    <p>Nombre: {item.nombre}</p>
+                                                    <h3 class="font-style: italic text-center">Compra</h3>
+                                                    <p class="font-['Open_Sans']">Nombre: {item.nombre}</p>
+                                                    {/* Probar cambiar las letras de todos estos parrafos */}
                                                     <p>Monto: {item.monto}</p>
                                                     <p>Cantidad Cuotas: {item.cantCuotas}</p>
                                                     <p>Fecha de Compra: {item.fechaCompra}</p>
@@ -178,10 +125,10 @@ function Registrador() {
                                                 </div>
                                             </div>
                                         </li>
-                                    </div>
-                                )
+                                    )
 
-                            }
+                                }
+                            </div>
                         </ul>
                     </div>
                 </div>
